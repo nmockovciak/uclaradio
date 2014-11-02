2d6e2104ea296bc34dc92fd9122b6a7bbe5422fb
hsuregan <hsuregan@gmail.com>
2014-11-01 19:38:22 -0700

    UTC and PST fix

--- js/scheduler.js
+++ js/scheduler.js
@@ -13,8 +13,21 @@
         var show = null;
         var time = new Date();
         var day = time.getUTCDay(); //0 corresponds to Sunday
-        time = time.getUTCHours() - 7 + (time.getMinutes())/60;
+        var minutes = time.getMinutes();
+        time = time.getUTCHours() - 7;
+        
+        if(time < 0) //saturday
+        {
+          time = 24 + time;
+          day = day -1;
+          if (day < 0)
+            day = 6;
+        }
+
+        time = time + minutes/60;
+
         console.log(time);
+        console.log(day);
         for (i = 0; i < data.length; i++) {
           if((data[i].day == day) && (data[i].start <= time) && (time <= data[i].end))
           {
@@ -24,7 +37,14 @@
         }
         document.getElementById("SHOW_TITLE").innerHTML = show.show;
         document.getElementById("DJ_NAME").innerHTML = show.dj;
-        $("#pic1").attr("src", show.image);
+        if(show.image != "") $("#pic1").attr("src", show.image);
+        else
+        {
+          var parent = document.getElementById("dj_image");
+          var child = document.getElementById("pic1");
+          parent.removeChild(child);
+        }
+
         document.getElementById("BLURB").innerHTML = show.description;
 
 
