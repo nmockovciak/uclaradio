4136a59e5507006e3f70255108421596ad7a6317
MatteoVH <matteovh@ucla.edu>
2015-05-08 17:43:39 -0700

    Added pledgedrive route.

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
 	res.render('index');
 });
 
+router.get('/pledgedrive', function(req, res, next) {
+	res.render('pledgedrive');
+});
+
 module.exports = router;
